d5fa2e68ee8311bf9eca2015abd8fd8f574067d3
Mòrag Burgon-Lyon <moragbl@users.noreply.github.com>
2016-01-09 19:51:22 +0000

    script to count the neighbouring pieces on a board

--- count-neighbours.py
+++ count-neighbours.py
@@ -3,20 +3,25 @@
 def count_neighbours(grid, row, col):
     max_rows = len(grid)
     max_cols = len(grid[0])
-    print "max_rows ", max_rows, "max_cols ", max_cols
     count = 0
+    if not row == 0:
+        count+=grid[row-1][col-1]
+        if not col ==0:
+            count+=grid[row-1][col]
+    if not col == 0:
+        count+=grid[row][col-1]
     if row < max_rows-1:
-        count+=grid[row+1][col-1]+grid[row+1][col]
-        print "count a ", count
+        count+=grid[row+1][col]
+        if not col == 0:
+            count+=grid[row+1][col-1]
     if col < max_cols-1:
-        count+=grid[row-1][col+1]+grid[row][col+1]
-        print "count b ", count
-    if (col < max_cols-1) and (row < max_rows-1):
-        count +=grid[row+1][col+1]
-        print "count c ", count
-    count  += grid[row-1][col-1]+grid[row-1][col]+grid[row][col-1]
-    print "count d ", count
+        if not row == 0:
+            count+=grid[row-1][col+1]
+        count+=grid[row][col+1]
+    if col < max_cols-1 and row < max_rows-1:
+        count+=grid[row+1][col+1]
     return count
+
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert count_neighbours(((1, 0, 0, 1, 0),
