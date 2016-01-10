546d54d905da57725a36a9708153f7c6c6fd445f
Mòrag Burgon-Lyon <moragbl@users.noreply.github.com>
2016-01-10 09:45:06 +0000

    2.7 version of most wanted letter, 3.3 version needs iterkeys instead of keys

---
+++ most-wanted-letter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import string
+
+def checkio(text):
+    letter_count = {}
+    for letter in list(string.ascii_lowercase):
+        letter_count[letter]= text.lower().count(letter)
+    return max(sorted(letter_count.keys()), key=(lambda k: letter_count[k]))
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
